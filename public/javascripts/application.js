c61c0dd437826c904b83bb3d8c148c3357143c89
Donald Ball <donald.ball@gmail.com>
2010-04-12 22:11:42 -0400

    tile highlighting

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,6 +2,7 @@
 // This file is automatically included by javascript_include_tag :defaults
 var Map = function(json_arg) {
   var json = json_arg;
+  var tiles = new Object();
   var canvas = document.getElementById('map');
   var context = canvas.getContext('2d');
   var tile_size = Math.min(
@@ -9,8 +10,37 @@ var Map = function(json_arg) {
     canvas.height / json.height
   );
 
+  function getTile(x, y) {
+    tile_x = Math.floor(x / tile_size);
+    tile_y = Math.floor(y / tile_size);
+    var key = '' + tile_x + ',' + tile_y;
+    var tile = tiles[key];
+    if (tile == null) {
+      tile = new Object();
+      tile.x = tile_x;
+      tile.y = tile_y;
+      tiles[key] = tile;
+    }
+    return tile;
+  }
+
+  function toggleHighlightTile(tile) {
+    var x = 1 + tile.x * tile_size;
+    var y = 1 + tile.y * tile_size;
+    var w = tile_size - 1;
+    var h = tile_size - 1;
+    if (tile.highlighted) {
+      context.clearRect(x, y, w, h);
+      tile.highlighted = false;
+    } else {
+      context.fillStyle = 'rgba(255, 0, 0, 0.25)';
+      context.fillRect(x, y, w, h);
+      tile.highlighted = true;
+    }
+  }
+
   function drawFigures() {
-    context.font = '' + map.tile_size + 'px courier';
+    context.font = '' + tile_size + 'px courier';
     context.textAlign = 'center';
     context.textBaseline = 'middle';
     for (var i=0; i < json.figures.length; i++) {
@@ -45,12 +75,15 @@ var Map = function(json_arg) {
   }
 
   return {
-    tile_size: tile_size,
-    context: context,
-    canvas: canvas,
     draw: function() {
+      context.save();
       drawGrid();
       drawFigures();
+      $('#map').click(function(evt) {
+        var tile = getTile(evt.pageX, evt.pageY);
+        toggleHighlightTile(tile);
+      });
+      context.restore();
     }
   }
 }

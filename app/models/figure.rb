3d108f187ebcd92474e4c7d1f2e327ebba178ea7
Donald Ball <donald.ball@gmail.com>
2010-04-13 19:41:16 -0400

    adding SIZES, size validation

--- app/models/figure.rb
+++ app/models/figure.rb
@@ -1,6 +1,9 @@
 class Figure < ActiveRecord::Base
+  SIZES = %w(fine diminuitive tiny small medium large huge gargantuan colossal)
+
   belongs_to :map
 
   validates_numericality_of :position_x
   validates_numericality_of :position_y
+  validates_inclusion_of :size, :in => SIZES.map {|s| s[0, 1] }
 end

87984b00cbbdc0a43ac033fa1b0d2e05f2717f7d
Donald Ball <donald.ball@gmail.com>
2010-05-01 21:14:59 -0400

    commenting out paperclip

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,14 +1,14 @@
 class Image < ActiveRecord::Base
   belongs_to :map
 
-  has_attached_file :image,
-    :storage => :s3,
-    :s3_credentials => {
-      :access_key_id => ENV['S3_KEY'],
-      :secret_access_key => ENV['S3_SECRET']
-    },
-    :bucket => "combat-#{Rails.env.to_s}",
-    :path => '/images/:id'
+#  has_attached_file :image,
+#    :storage => :s3,
+#    :s3_credentials => {
+#      :access_key_id => ENV['S3_KEY'],
+#      :secret_access_key => ENV['S3_SECRET']
+#    },
+#    :bucket => "combat-#{Rails.env.to_s}",
+#    :path => '/images/:id'
 
   def url
     image.url

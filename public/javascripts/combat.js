8eda5e965462dd6bfa46ce84f816dc38ac81a195
Donald Ball <donald.ball@gmail.com>
2010-08-07 22:24:07 -0400

    to_json changed behavior between r3b3 and r3rc

--- public/javascripts/combat.js
+++ public/javascripts/combat.js
@@ -2,10 +2,10 @@ var Combat = {
   url: window.location.href,
 
   init: function(json, id, viewport_id) {
-    this.map.init(json, id, viewport_id);
-    this.figures.init(json.figures);
-    this.pictures.init(json.images);
-    this.walls.init(json.walls);
+    this.map.init(json.map, id, viewport_id);
+    this.figures.init(json.map.figures);
+    this.pictures.init(json.map.images);
+    this.walls.init(json.map.walls);
     this.map.viewport.reset();
     this.actions.bind($('#' + id));
   },

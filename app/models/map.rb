082b49590227237e07a26112a27d428b52eadf6d
Donald Ball <donald.ball@gmail.com>
2010-08-23 23:10:18 -0400

    Explicitly order map children

--- app/models/map.rb
+++ app/models/map.rb
@@ -1,9 +1,9 @@
 class Map < ActiveRecord::Base
   has_many :viewports, :dependent => :destroy
-  has_many :figures, :dependent => :destroy, :inverse_of => :map
-  has_many :walls, :dependent => :destroy
-  has_many :images, :dependent => :destroy
-  has_many :effects, :dependent => :destroy, :inverse_of => :map
+  has_many :figures, :dependent => :destroy, :inverse_of => :map, :order => 'figures.id'
+  has_many :walls, :dependent => :destroy, :order => 'walls.id'
+  has_many :images, :dependent => :destroy, :order => 'images.id'
+  has_many :effects, :dependent => :destroy, :inverse_of => :map, :order => 'effects.id'
 
   validates_presence_of :name
 

82a6d6d9e593503bfbe51c79cb03d96c19ef9af9
Donald Ball <donald.ball@gmail.com>
2011-04-08 14:35:54 -0400

    Add kinds to seed colors

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
 palette = Palette.create!
 palette.colors.clear
 [
-  [0, 0, 0],
-  [255, 0, 0],
-  [0, 255, 0],
-  [0xa5, 0x2a, 0x2a]
-].each do |red, green, blue|
-  palette.colors.create!(:red => red, :green => green, :blue => blue, :alpha => 1)
+  [0, 0, 0, 'prop'],
+  [255, 0, 0, 'extra'],
+  [0, 255, 0, 'actor'],
+  [0xa5, 0x2a, 0x2a, 'set']
+].each do |red, green, blue, kind|
+  palette.colors.create!(:red => red, :green => green, :blue => blue, :alpha => 1, :kind => kind)
 end
